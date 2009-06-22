8f155f1eae98a3282c4dddb2dda9a46fea8cee1f
System Administrator <root@jsmyth-mbp.(none)>
2009-06-22 01:10:38 -0700

    Added gems to the app

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,13 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'searchlogic'
+  config.gem 'rr'
+  config.gem 'quietbacktrace'
+  config.gem 'thoughtbot-shoulda', :lib => 'shoulda'
+  config.gem 'chriseppstein-compass', :lib => 'compass', :version => '>= 0.6.6'
+  config.gem 'haml-edge', :lib => 'haml', :version => '>= 2.1.3'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com', :version => '>= 2.2.3'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
