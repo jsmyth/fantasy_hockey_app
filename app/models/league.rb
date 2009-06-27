de6ea38ebbf6ae9e64b60308dfa7cab0374f5570
Jeremy Pruitt <jeremypruitt@me.com>
2009-06-27 16:00:52 -0700

    Fixing a typo of has_many fantsay_teams in the League model

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,7 +1,7 @@
 class League < ActiveRecord::Base
   has_and_belongs_to_many :stat_categories
   
-  has_many :fantsay_teams
+  has_many :fantasy_teams
   has_many :role_assignments
   has_many :roles, :through => :role_assignments
   has_many :users, :through => :role_assignments
