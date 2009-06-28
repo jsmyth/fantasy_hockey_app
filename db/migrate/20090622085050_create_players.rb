94313a6e21781fedeaf5802b6bae99f671f56efa
Jim Smyth <jsmyth@jsmyth-mbp.jnpr.net>
2009-06-27 22:25:19 -0700

    added more players in roster assignments seed data for other leagues and added nhl_team_abbreviation to player model

--- db/migrate/20090622085050_create_players.rb
+++ db/migrate/20090622085050_create_players.rb
@@ -1,10 +1,11 @@
 class CreatePlayers < ActiveRecord::Migration
   def self.up
     create_table :players do |t|
-      t.integer :nhl_team_id
+      t.integer :nhl_team_id      
       t.integer :beacon_id
       t.string :first_name
       t.string :last_name
+      t.string :nhl_team_abbreviation
       t.timestamps
     end
   end
