8d0a180e9e5c714bb76cb94eadfc92062a6a976d
Jim Smyth <jsmyth@jsmyth-mbp.(none)>
2009-09-08 20:38:36 -0700

    fixed season_id inside migration to read nhl_season_id

--- db/migrate/20090909014136_add_nhl_season_id_to_games.rb
+++ db/migrate/20090909014136_add_nhl_season_id_to_games.rb
@@ -1,9 +1,9 @@
 class AddNhlSeasonIdToGames < ActiveRecord::Migration
   def self.up
-    add_column :games, :season_id, :integer
+    add_column :games, :nhl_season_id, :integer
   end
 
   def self.down
-    remove_column :games, :season_id
+    remove_column :games, :nhl_season_id
   end
 end
