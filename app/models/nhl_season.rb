3bbace4bf00120d31a37a9fa443c8b72db3b3f1c
Jeremy Pruitt <jeremypruitt@me.com>
2009-09-10 20:35:28 -0700

    Adding a virtual attribute called name that takes the begins_on date and turns it into a '2009-10' style for use in views and dropdowns and such

--- app/models/nhl_season.rb
+++ app/models/nhl_season.rb
@@ -1,4 +1,18 @@
 class NhlSeason < ActiveRecord::Base
   has_many :games
   has_many :fantasy_seasons
+  
+  def name
+    # Being by grabbing just the year from the begins_on date
+    year = begins_on.year
+    
+    # Add 1 to the year and convert to string
+    next_year = (year + 1).to_s
+    
+    # And now a substring to grab just the last 2 numbers of the year
+    short_next_year = next_year[2,2]
+    
+    # And finally we return the NHL season name
+    "#{year}-#{short_next_year}"
+  end
 end
