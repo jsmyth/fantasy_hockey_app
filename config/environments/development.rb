052f652d23e6152308db8135ab2da769d088c63f
Jeremy Pruitt <jeremypruitt@me.com>
2009-09-10 20:24:10 -0700

    Adding bullet and ruby growl to the app. Bullet gives recommendations on how to reduce database queries.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,12 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+config.after_initialize do
+  Bullet.enable = true 
+  #Bullet::Association.alert = true
+  #Bullet::Association.bullet_logger = true  
+  Bullet::Association.console = true
+  Bullet::Association.growl = true
+  Bullet::Association.rails_logger = true
+end
