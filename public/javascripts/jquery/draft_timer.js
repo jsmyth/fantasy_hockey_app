bdc349ef35be0bb9db1748c1ee4a88580e95a1ea
Jeremy Pruitt <jeremypruitt@me.com>
2009-09-19 05:45:02 -0700

    The draft timer is now has analog-style flip numbers.

--- public/javascripts/jquery/draft_timer.js
+++ public/javascripts/jquery/draft_timer.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(function(){
-    $('#epiClock').epiclock({mode: EC_STOPWATCH, format: "i{m } s{s}"});   // Creates a text clock displaying local time
+    $('#epiClock').epiclock({mode: EC_STOPWATCH, format: "i:s"}, "retro");   // Creates a text clock displaying local time
     $.epiclock();                // Starts all the clocks you've created
   });
 });
