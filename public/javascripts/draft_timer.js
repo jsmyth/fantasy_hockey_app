3f821606a15b6ef6fb62976b3bcca7ce83b3ce14
Jeremy Pruitt <jeremypruitt@me.com>
2009-09-26 03:11:34 -0700

    Moved jquery_timer out of the jquery folder.

--- public/javascripts/draft_timer.js
+++ public/javascripts/draft_timer.js
@@ -3,13 +3,4 @@ $(document).ready(function() {
     $('#epiClock').epiclock({mode: EC_STOPWATCH, format: "i:s"}, "retro");   // Creates a text clock displaying local time
     $.epiclock();                // Starts all the clocks you've created
   });
-  
-  $('table#draft_table tbody tr').quicksearch({
-    position: 'before',
-    attached: '#liveSearch',
-    stripeRowClass: ['odd', 'even'],
-    labelText: '',
-    delay: 100,
-    loaderText: ''
-  });
 });
