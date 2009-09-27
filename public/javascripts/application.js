594687941a4fe8581e2cbda7302cb57f0d164660
Jeremy Pruitt <jeremypruitt@me.com>
2009-09-27 01:36:17 -0700

    Adding an alternate view for the players table. It uses jquery to toggle between the big rows with the photos and much smaller rows with no pictures and with stats.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -33,4 +33,20 @@ $(document).ready(function() {
     new_src = $(this).find("img").attr("src").replace(/reverse/, 'normal');
     $(this).find("img.number").attr("src", new_src)   }
   );
+  
+  $("a.switch-view").toggle(function(){
+    $(".player-photo").addClass("display-none");
+    $(".row").addClass("small-row");
+    $(".player-photo").removeClass("small-photo");
+    $(".player-stat").removeClass("display-none");
+    $(".nhl-team-name").removeClass("display-none");
+    $(".nhl-team-photo").addClass("display-none");
+  }, function () {
+    $(".player-photo").removeClass("display-none");
+    $(".row").removeClass("small-row");
+    $(".player-photo").addClass("small-photo");
+    $(".player-stat").addClass("display-none");
+    $(".nhl-team-name").addClass("display-none");
+    $(".nhl-team-photo").removeClass("display-none");
+  });
 });
