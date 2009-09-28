14c8d345b3f9018d50540336828874f427fb12b5
Jeremy Pruitt <jeremypruitt@me.com>
2009-09-27 17:07:54 -0700

    Moved the ajax setup javascript to it's own initalize.js file so it can be loaded before the other libraries and allow the application.js to be loaded after the other libraries.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,25 +1,4 @@
-// This sets up the proper header for rails to understand the request type,
-// and therefore properly respond to js requests (via respond_to block, for example)
-$.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-$.fn.pause = function(duration) {
-    $(this).animate({ dummy: 1 }, duration);
-    return this;
-};
 $(document).ready(function() {
-  // UJS authenticity token fix: add the authenticy_token parameter
-  // expected by any Rails POST request.
-  $(document).ajaxSend(function(event, request, settings) {
-    // do nothing if this is a GET request. Rails doesn't need
-    // the authenticity token, and IE converts the request method
-    // to POST, just because - with love from redmond.
-    if (settings.type == 'GET') return;
-    if (typeof(AUTH_TOKEN) == "undefined") return;
-    settings.data = settings.data || "";
-    settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
-  });
-  
   $('#flash_content').pause(5000).slideUp('slow');
   
   $("tr.row").hover(
