a9c2e4920c6a4692240a86bc79f24a297ed56360
Jim Smyth <jsmyth@jsmyth-mbp.jnpr.net>
2010-08-17 10:16:10 -0700

    commenting out acts_as_list as it is not being used at the moment and it's causing some issues with the dev data

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,6 +2,6 @@ class Participation < ActiveRecord::Base
   belongs_to :fantasy_season
   belongs_to :fantasy_team
 
-  acts_as_list :scope => :fantasy_season
+  #acts_as_list :scope => :fantasy_season
   default_scope :order => :position
 end
